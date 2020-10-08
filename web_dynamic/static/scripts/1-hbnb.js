c992a267f2e616175ccbac483c681f820fa746c4
AngieCastano1634 <1634@holbertonschool.com>
2020-10-08 02:29:41 +0000

    [CHANGE][h3 updates when clicking but the title Amenities is lost]

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,11 @@
-function AppendItemsToList()
-{
-	console.log("Sirvo");
+function CheckedList(){
+	HtmlInput = document.getElementsByTagName("INPUT");
+    	$(".amenities h3").empty();
+	for(var item of HtmlInput){
+		let item1 = $("<li></li>").text(item.dataset.name);
+		if(item.checked){
+			$(".amenities h3").append(item1)
+		}
+	}
+	
 }

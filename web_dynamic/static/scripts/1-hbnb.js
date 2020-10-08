f84421aafae3f9a6ddaf1d7cf84193fbee586a83
AngieCastano1634 <1634@holbertonschool.com>
2020-10-08 04:06:44 +0000

    [ADD][Not overflow]

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,28 @@
+function NotOverflow() {
+	let leng = 0;
+	let maximum = 13;
+	H_4_to_add = []
+	let i = 0;
+	for (i = 0; leng < maximum && H_4[i]; i++){
+		leng += H_4[i].length;
+		H_4_to_add.push(H_4[i]);
+	}
+	let = remaining = maximum - leng;    
+	if (H_4[i]){
+		H_4_to_add.push(H_4[i].slice(0, remaining));
+	}
+	H_4_to_add.push("...");
+  document.getElementById("demo").innerHTML = H_4_to_add;
+}
 function CheckedList(){
 	HtmlInput = document.getElementsByTagName("INPUT");
     	$(".amenities h4").empty();
+	H_4 = []
 	for(var item of HtmlInput){
 		let item1 = $("<li></li>").text(item.dataset.name);
 		if(item.checked){
-			$(".amenities h4").append(item1)
+			H_4.push(item.dataset.name)
 		}
 	}
-	
+	NotOverflow()	
 }

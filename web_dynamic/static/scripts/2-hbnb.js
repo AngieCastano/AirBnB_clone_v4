2f7c5ea858c825d443edceccc0376889bc3fe813
AngieCastano1634 <1634@holbertonschool.com>
2020-10-09 02:42:39 +0000

    [MIX][API + filter]

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,26 +1,33 @@
 // Script that is executed only when DOM is loaded with jQuery
 
-let checked_box = {};
-$(document).ready(function () {
-    $('input:checkbox').change(function () {
-	if ($(this).is(':checked_box')) {
-	        checked_box[$(this).data('id')] = $(this).data('name');
-	    }
-	else {
-	        delete checked_box[$(this).data('id')];
-	    }
-	$('div.amenities h4').html(function () {
-	        let amenities = [];
-	        Object.keys(checked_box).forEach(function (key) {
-		    amenities.push(checked_box[key]);
-		        });
-	        if (amenities.length === 0) {
-		    return ('&nbsp');
-		        }
-	        return (amenities.join(', '));
-	    });
-    });
-
+function NotOverflow() {
+	let leng = 0;
+	let maximum = 13;
+	H_4_to_add = []
+	let i = 0;
+	for (i = 0; leng < maximum && H_4[i]; i++){
+		leng += H_4[i].length;
+		H_4_to_add.push(H_4[i]);
+	}
+	let = remaining = maximum - leng;    
+	if (H_4[i]){
+		H_4_to_add.push(H_4[i].slice(0, remaining));
+	}
+	H_4_to_add.push("...");
+  document.getElementById("amenities_h4").innerHTML = H_4_to_add;
+}
+function CheckedList(){
+	HtmlInput = document.getElementsByTagName("INPUT");
+    	$(".amenities h4").empty();
+	H_4 = []
+	for(var item of HtmlInput){
+		let item1 = $("<li></li>").text(item.dataset.name);
+		if(item.checked){
+			H_4.push(item.dataset.name)
+		}
+	}
+	NotOverflow()	
+}
 
 const apiStatus = $('DIV#api_status');
 $.ajax('http://0.0.0.0:5001/api/v1/status/').done(function (data) {
